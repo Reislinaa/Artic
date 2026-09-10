d338680c44801e769285453e6dac8cbfac7ab311
artic <artic@local>
2026-09-10 15:27:24 +0800

    chore: 清理误提交的 PDF 解析临时文件 + .gitignore 增加 _* 防复发规则

--- _pdf_probe.py
+++
@@ -1,8 +0,0 @@
-import importlib
-
-for m in ['fitz', 'pypdf', 'PyPDF2', 'pdfplumber', 'pdfminer', 'PIL']:
-    try:
-        mod = importlib.import_module(m)
-        print('OK   %-12s %s' % (m, getattr(mod, '__version__', '')))
-    except Exception as e:
-        print('--   %-12s %s' % (m, type(e).__name__))
